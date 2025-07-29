2b459bb70e1e88fb6a111e2d359dcfd16bad8b47
oybekmalikov <162029195+oybekmalikov@users.noreply.github.com>
2025-07-29 10:27:14 +0300

    done

--- src/components/group/group-students.tsx
+++ src/components/group/group-students.tsx
@@ -59,7 +59,7 @@ const GroupStudent = ({ students, id }: any) => {
 				<div className="overflow-x-auto overflow-y-auto max-h-[500px] [&::-webkit-scrollbar]:hidden">
 					<table className="w-full">
 						<thead className="bg-gray-50">
-							<tr className="sticky top-0 bg-gray-100">
+							<tr className="sticky top-0 bg-white">
 								<th className="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
 									Students
 								</th>
